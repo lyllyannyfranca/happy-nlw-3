db373dac2209acefd5054607d40e590739c4a893
Lyllyanny França <lyllyanny.franca@gmail.com>
2020-10-15 19:48:22 -0300

    quarto dia de NLW#3 finalizado

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -7,21 +7,21 @@ const options = {
 }
 
 //create map
-const map = L.map('mapid', options).setView([-27.222633,-49.6455874], 15);
+const map = L.map('mapid', options).setView([-8.0514722,-34.9394314], 15);
 
 //create and add tileLayer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 //create icon
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
+    iconUrl: "/images/map-marker.svg",
     iconSize: [58, 68],
     iconAnchor: [29, 68],
     popupAnchor: [170, 2]
 });
 
 //create and add marker
-L.marker([-27.222633,-49.6455874], { icon }).addTo(map)
+L.marker([-8.0514722,-34.9394314], { icon }).addTo(map)
 
 /* image gallery */
 
---
+++ src/database/fakedata.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-8.0514722",
+        lng: "-34.9394314",
+        name: "Lar das meninas",
+        description: "Presta assitência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1596464716127-f2a82984de30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+            
+            "https://images.unsplash.com/photo-1533222481259-ce20eda1e20b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 8h até 18hs",
+        open_on_weekends: "1"
+    },
+    {
+        id: 2,
+        lat: "-8.0514722",
+        lng: "-34.9494314",
+        name: "Lar dos meninos",
+        description: "Presta assitência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1596464716127-f2a82984de30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+            
+            "https://images.unsplash.com/photo-1533222481259-ce20eda1e20b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 8h até 18hs",
+        open_on_weekends: "0"
+    },
+]
